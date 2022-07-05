db2ff8e08ca38ed4e800703e6ed78d22891fcc4f
chatterboy <cahtter0502@gmail.com>
2022-07-05 15:42:23 +0900

    Add basic modules

---
+++ agts/preprocessing.py
@@ -0,0 +1,37 @@
+def interpolate():
+    """
+    interpolation 함수는 SciPy와 Pandas에서 찾을 수 있음.
+    특히, SciPy는 다양한 interpolation 함수가 있음. 함수 간에
+    어떤 차이가 있는지 구현을 해야하는지 확인할 필요가 있음.
+
+    :return:
+    """
+    pass
+
+
+def impute():
+    """
+    imputation 함수는 sklearn에서 찾을 수 있음.
+    다양한 imputation 함수를 지원하고 있음.
+    TODO: 함수 간 차이점 확인
+    TODO: 함수별 구현 필요성 확인
+
+    :return:
+    """
+    pass
+
+
+def scale():
+    """
+    scaling 함수는 sklearn.preprocessing에서 찾을 수 있음.
+    다양한 scaling 함수를 지원하고 있음.
+    TODO: 함수 간 차이점 확인
+    TODO: 함수별 구현 필요성 확인
+
+    :return:
+    """
+    pass
+
+
+def minmax_scale():
+    pass
---
+++ agts/timeseries.py
@@ -0,0 +1,28 @@
+class TimeSeries:
+    def __init__(self,
+                 data):
+        self.data = data
+
+    def time(self):
+        pass
+
+    def value(self):
+        pass
+
+    def min(self):
+        pass
+
+    def max(self):
+        pass
+
+    def has_missing(self):
+        pass
+
+    def is_univariate(self):
+        pass
+
+    def to_array(self):
+        pass
+
+    def to_tensor(self):
+        pass
